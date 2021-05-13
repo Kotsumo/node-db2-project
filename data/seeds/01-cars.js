72aa1d4a0f7a7397c383add5b71ed5fcb246ae71
David <davidellis1020@gmail.com>
2021-05-13 11:10:55 -0700

    setup project with seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,48 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'supra',
+        mileage: 10000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '2222222222222',
+        make: 'honda',
+        model: 'prelude',
+        mileage: 15000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '3333333333333',
+        make: 'nissan',
+        model: 'skyline R34',
+        mileage: 25000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '4444444444444',
+        make: 'mazda',
+        model: 'rx-7 fd3s',
+        mileage: 2000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '5555555555555',
+        make: 'toyota',
+        model: '86',
+        mileage: 5000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
